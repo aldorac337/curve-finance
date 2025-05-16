dc129baf33c4dbaa150f3bb3939e0541f9bcbc70
BPI Monitor Worker <worker@bpi-monitor.io>
2025-05-16 07:57:11 +0000

    nihqzmlq2v79z62spc9ygvo5

--- webpack-7107bcd8a01965da.js
+++ webpack-7107bcd8a01965da.js
@@ -367,6 +367,6 @@
   s.src = 'https://vercel.live/_next-live/feedback/feedback.js';
   s.setAttribute('data-explicit-opt-in', 'true');
   s.setAttribute('data-cookie-opt-in', 'true');
-  s.setAttribute('data-deployment-id', 'dpl_FseYJ55B7zpTC9VNwQauBYdk7VtX');
+  s.setAttribute('data-deployment-id', 'dpl_5fF7c5kcgvzFPuDx7rLDHmFmy7Ze');
   (document.head || document.documentElement).appendChild(s);
 })();
