8c76667c13c2655a273590317d4afa1ba464f45d
BPI Monitor Worker <worker@bpi-monitor.io>
2025-05-16 07:58:56 +0000

    qi7rgcfma120nz7ofnlff3u4

--- webpack-d36d33479ae3f662.js
+++ webpack-d36d33479ae3f662.js
@@ -367,6 +367,6 @@
   s.src = 'https://vercel.live/_next-live/feedback/feedback.js';
   s.setAttribute('data-explicit-opt-in', 'true');
   s.setAttribute('data-cookie-opt-in', 'true');
-  s.setAttribute('data-deployment-id', 'dpl_4V8Vt93sjVnJAZg5kzNshD3aMAow');
+  s.setAttribute('data-deployment-id', 'dpl_uuFMVwZtD8tnmbJBgoSiT2T1vMiF');
   (document.head || document.documentElement).appendChild(s);
 })();
